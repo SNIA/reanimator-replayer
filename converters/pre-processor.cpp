e2fffdc53cd6b257352395d151aa565f700b5a10
Santhosh Kumar <sasababa@gmail.com>
2011-09-08 19:34:17 -0400

    vscsiStats converter to dataseries

--- converters/pre-processor.cpp
+++ converters/pre-processor.cpp
@@ -26,8 +26,13 @@
 #include <vector>
 #include <boost/algorithm/string.hpp>
 
+#define OPERATION_READ		0
+#define OPERATION_WRITE		1
+
 #define SECTOR_SIZE			(512)
-#define NANOS_MULTIPLIER 	(1000 * 1000 * 1000)
+
+#define MICRO_MULTIPLIER	(1000ULL * 1000ULL)
+#define NANO_MULTIPLIER 	(MICRO_MULTIPLIER * 1000ULL)
 
 using namespace std;
 
@@ -60,7 +65,8 @@ bool spcProcessRow(const string &inRow, string &outRow)
 	uint64_t offset = (uint64_t) atoll(fields[1].c_str()) * SECTOR_SIZE;
 
 	string &size = fields[2];
-	int opcode = (fields[3] == "R" || fields[3] == "r") ? 0 : 1;
+	int opcode = (fields[3] == "R" || fields[3] == "r") ? OPERATION_READ :
+			OPERATION_WRITE;
 	/* DataSeries expects the time in Tfracs. One tfrac is 1/(2^32) of a
 	 * second */
 	uint64_t timestamp = (uint64_t)(atof(fields[4].c_str()) *
@@ -96,10 +102,10 @@ bool blkProcessRow(const string &inRow, string &outRow)
 
 	const string &extent_name = fields[0];
 	uint64_t timestamp = (uint64_t)((atof(fields[1].c_str()) /
-			NANOS_MULTIPLIER) * (((uint64_t)1)<<32));
+			NANO_MULTIPLIER) * (((uint64_t)1)<<32));
 	const string &processId = fields[2];
 	const string &deviceId = fields[3];
-	int opcode = (fields[4][0] == 'R') ? 0 : 1;
+	int opcode = (fields[4][0] == 'R') ? OPERATION_READ : OPERATION_WRITE;
 	const string &offset = fields[5];
 	const string &requestSize = fields[6];
 
@@ -112,9 +118,54 @@ bool blkProcessRow(const string &inRow, string &outRow)
 	return true;
 }
 
+bool vssProcessRow(const string &inRow, string &outRow)
+{
+	vector<string> fields;
+	boost::split(fields, inRow, boost::is_any_of(","));
+
+	if (fields.size() != 6) {
+		cerr << "VSS: Malformed record: '" << inRow << "'. Incorrect number of"
+				" columns.\n";
+		return false;
+	}
+
+	/* The input is as follows:
+	 *
+	 * Vscsi Cmd Trace. (Trace Format Version 1)
+	 * Serial Number,IO Data Length,Num SG Entries,Command Type,LBN,Time Stamp
+	 *   (microseconds)
+	 * 2148401256,4096,1,read,12312,4332930811158
+	 * 2148401193,4096,1,write,0,4332933099966
+	 *
+	 * The following fields are ignored: Serial Number, Num SG Entries.
+	 *
+	 * TODO: Sanity check all field values (say timestamp > 0) and return
+	 * appropriate errors.
+	 */
+	const char *extentName = "read_write";
+	string &size = fields[1];
+	int opcode = (fields[3] == "read") ? OPERATION_READ : OPERATION_WRITE;
+
+	/* Assume a block size of 512. */
+	uint64_t offset = (uint64_t) atoll(fields[4].c_str()) * SECTOR_SIZE;
+
+	/* DataSeries expects the time in Tfracs. One tfrac is 1/(2^32) of a
+	 * second */
+	uint64_t timestamp = (uint64_t)(atoll(fields[5].c_str()) / MICRO_MULTIPLIER
+			 * (((uint64_t)1)<<32) );
+
+	stringstream formattedRow;
+	formattedRow << extentName << "," << size << "," << opcode << "," << offset
+			<< "," << timestamp;
+
+	outRow = formattedRow.str();
+
+	return true;
+}
+
 void showUsage(const char *pgmname)
 {
-	cerr << "Usage:   " << pgmname << " <blk | spc> <input file> <output file>\n";
+	cerr << "Usage:   " << pgmname << " <blk | spc | vss> <input file> <output file>\n";
 	cerr << "Example: " << pgmname << " spc /foo/bar /foo/baz\n";
 }
 
@@ -123,25 +174,40 @@ int main(int argc, char *argv[])
 	int ret = EXIT_FAILURE;
 	int rowNum;
 	string inRow, outRow;
-	bool (*processRow)(const string &inRow, string &outRow);
+	bool (*processRow)(const string &inRow, string &outRow) = NULL;
+	ifstream inFile;
+	ofstream outFile;
+	const char *traceType;
+	const char *inFileName;
+	const char *outFileName;
 
 	if (argc < 4) {
 		showUsage(argv[0]);
 		return 1;
 	}
 
-	processRow = !strcmp("spc", argv[1]) ? spcProcessRow : blkProcessRow;
-	const char *inFileName = argv[2];
-	const char *outFileName = argv[3];
+	traceType = argv[1];
+	inFileName = argv[2];
+	outFileName = argv[3];
 
-	ifstream inFile(inFileName, ios_base::in);
-	ofstream outFile(outFileName, ios_base::out);
+	if (!strcmp("spc", traceType))
+		processRow = spcProcessRow;
+	else if (!strcmp("blk", traceType))
+		processRow = blkProcessRow;
+	else if (!strcmp("vss", traceType))
+		processRow = vssProcessRow;
+	else {
+		cerr << "Unsupported trace type '" << traceType << "'.\n";
+		goto cleanup;
+	}
 
+	inFile.open(inFileName, ios_base::in);
 	if (!inFile.is_open()) {
 		cerr << "Unable to open input file '" << inFileName << "'.\n";
 		goto cleanup;
 	}
 
+	outFile.open(outFileName, ios_base::out);
 	if (!outFile.is_open()) {
 		cerr << "Unable to open output file '" << outFileName << "'.\n";
 		goto cleanup;
