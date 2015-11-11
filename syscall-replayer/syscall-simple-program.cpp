b624d5e1dc02257ffeb77e6847c51533d7a17826
Leixiang Wu <leixiang.wu@stonybrook.edu>
2015-11-10 19:06:18 -0500

    Rename the simple program and add a write system call feature.

--- syscall-replayer/syscall-simple-program.cpp
+++ syscall-replayer/syscall-simple-program.cpp
@@ -15,30 +15,43 @@
 #include <fcntl.h>
 
 int main(int argc, char *argv[]){
-  if (argc < 2) {
+  if (argc < 3) {
     std::cerr << "Too few arguments!" << std::endl;
     std::cerr << "Usage: syscall-simple-read <filename>" << std::endl;
     return 1;
   }
 
-  // open the input file
-  int fd = open(argv[1], O_RDONLY);
-  if (fd < -1){
+  // open the input and output file
+  int infd = open(argv[1], O_RDONLY);
+  int outfd = open(argv[2], O_WRONLY);
+  if (infd < -1){
     perror(argv[1]);
+    return 1;
+  }
+
+  if (outfd < -1){
+    perror(argv[2]);
     return 1;
   }
 
   int page_size = getpagesize();
   char buffer[page_size];
-	
-  int bytes_read = read(fd, buffer, page_size);
-
+  
+  int bytes_read = read(infd, buffer, page_size);
+  int bytes_write = write(outfd, buffer, bytes_read);
+  
   while(bytes_read > 0){
     std::cout << buffer;
-    bytes_read = read(fd, buffer, page_size);
+    bytes_read = read(infd, buffer, page_size);
+    bytes_write = write(outfd, buffer, bytes_read);
+    if (bytes_write != bytes_read) {
+      return 0;
+    }
   }
-
-  close(fd);
+  
+  close(infd);
+  close(outfd);
+  
   return 0;
 }
 
