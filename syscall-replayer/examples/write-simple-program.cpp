c8a3f871b3607276d490c5a43af956924021a3b6
Shubhi Rani <shubhi@dhcp214.fsl.cs.sunysb.edu>
2015-11-30 01:13:50 -0500

    Added write-simple-program.cpp and Makefile for examples directory

--- syscall-replayer/examples/write-simple-program.cpp
+++ syscall-replayer/examples/write-simple-program.cpp
@@ -21,14 +21,48 @@
 #define ITR 1000
 
 int main(int argc, char *argv[]){
-  if (argc < 2) {
+
+  extern char *optarg;
+  int flag=0, iterations=0;
+  while(1){
+                int c ;
+                c= getopt(argc, argv, ":n:h");
+                if (c == -1)
+                        break;
+                switch(c){
+                        case 'n':
+                                iterations = atoi(optarg);
+                                flag +=1;
+                                break;
+			case 'h':
+                                std::cout << "Use -n option to provide #iterations" << std::endl;
+                                break;
+   			case '?':
+                                std::cerr << "Unknown option character" << optopt << std::endl;
+                                return 1;
+                        default:
+                                break;
+                }
+        }
+
+  if(flag == 0){
+                std::cout << "Please specify number of ITERATIONS" << std::endl;
+                exit(2);
+        }
+
+  if(flag > 1){
+                std::cout << "-n option is given twice" << std::endl;
+                exit(2);
+        }
+
+  if (argc < 4) {
     std::cerr << "Too few arguments!" << std::endl;
-    std::cerr << "Usage: strace-write <filename>" << std::endl;
+    std::cerr << "Usage: write-simple-program <-n #iterations> <filename>" << std::endl;
     return 1;
   }
 
   // open the output file
-  int outfd = open(argv[1], O_WRONLY);
+  int outfd = open(argv[3], O_WRONLY);
 
   if (outfd < -1){
     perror(argv[1]);
@@ -40,57 +74,25 @@ int main(int argc, char *argv[]){
 
   int page_size = getpagesize();
   char buffer[page_size];
-  memset(buffer,'0',page_size);
+  memset(buffer,'5',page_size);
 
-  int bytes_write;
+  int bytes_write =0;
 
+    
   /* To write zeros */
   start = time(NULL);
-  while(i < ITR){
+  while(i < iterations){
     bytes_write = write(outfd, buffer, page_size);
     i++;
+    if(bytes_write < 0)
+	std::cout << "Write Error!" << std::endl;
   }
+  
   end = time(NULL);
   std::cout << "Starting  Time:" << start << std::endl;
   std::cout << "Finishing Time:" << end << std::endl;
   std::cout << "Time taken in writing zeros:" << (end - start) << std::endl;
 
-  #if 0
-  /* To write fixed pattern */
-  i =0;
-  memset(buffer,'5',page_size);
-  start = time(NULL);
-  while(i < ITR){
-    bytes_write = write(outfd, buffer, page_size);
-    i++;
-  }
-  end = time(NULL);
-  std::cout << "Starting  Time:" << start << std::endl;
-  std::cout << "Finishing Time:" << end << std::endl;
-  std::cout << "Time taken in writing fixed pattern:" << (end - start) << std::endl;
-
-  /* To write random pattern */
-  i=0;
-  std::ifstream random_file_;
-  random_file_.open("/dev/urandom");
-  if (!random_file_.is_open()) {
-    std::cerr << "Unable to open file '/dev/urandom/'.\n";
-    exit(EXIT_FAILURE);
-  }
-
-  start = time(NULL);
-  while(i < ITR){
-    random_file_.read(buffer, page_size);
-    bytes_write = write(outfd, buffer, page_size);
-    i++;
-  }
-  end = time(NULL);
-  std::cout << "Starting  Time:" << start << std::endl;
-  std::cout << "Finishing Time:" << end << std::endl;
-  std::cout << "Time taken in writing random pattern:" << (end - start) << std::endl;
-
-  random_file_.close();
-  #endif
   close(outfd);
 
   return 0;
