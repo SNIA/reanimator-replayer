89d2763cdd06fe40dca2f1863bc8cefab8cba444
Leixiang Wu <leixiang.wu@stonybrook.edu>
2016-02-24 14:47:57 -0500

    Modify Makefile to build csv2ds that is moved from block converter. Modify strace2csv to parse errno in strace and output the errno num in CSV Copy csv2ds program from block converter directory into sys converter directory. Fix coding style of csv2ds.

---
+++ syscall-converter/csv2ds.cpp
@@ -0,0 +1,333 @@
+/*
+ * Copyright (c) 2015-2016 Leixiang Wu
+ * Copyright (c) 2015-2016 Shubhi Rani
+ * Copyright (c) 2015-2016 Sonam Mandal
+ * Copyright (c) 2015-2016 Erez Zadok
+ * Copyright (c) 2015-2016 Stony Brook University
+ * Copyright (c) 2011 Jack Ma
+ * Copyright (c) 2011 Vasily Tarasov
+ * Copyright (c) 2011 Santhosh Kumar Koundinya 
+ * Copyright (c) 2011 Geoff Kuenning
+ * Copyright (c) 2011 Harvey Mudd College 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * csv2ds.
+ * This program is similar to DataSeries's csv2ds utility, but handles
+ * extents with different types and nullable fields and is primarily used 
+ * for converting system call csv traces.
+ * 
+ * Usage: ./csv2ds <outputfile> <tablefile> <spec_string_file> 
+ *        <xml directory> <inputfiles...>
+ *
+ * <outputfile>: name of the dataseries output file
+ * <tablefile>: name of the table file to refer to
+ * <spec_string_file>: name of the file that contains a string 
+ *                    that specifies the format of the input file
+ * <xml directory>: directory path that contains extent xml. 
+ *                 Remember to '/' should be the last character.
+ * <inputfiles...>: input CSV files
+ */
+
+#include <cassert>
+
+#include <iostream>
+#include <fstream>
+#include <boost/algorithm/string.hpp>
+#include <boost/tokenizer.hpp>
+
+#include <DataSeries/ExtentType.hpp>
+#include <DataSeries/DataSeriesFile.hpp>
+#include <DataSeries/DataSeriesModule.hpp>
+
+#include "FieldSpace.hpp"
+
+using namespace std;
+
+/* map<fieldname, pair<nullable, ExtentType> */
+typedef map<string, pair<bool, ExtentType::fieldType> > config_table_entry_type;
+/* map<extentname, config_table_entry_type> */
+typedef map<string,config_table_entry_type > config_table_type;
+
+typedef map<string, OutputModule*> OutputModule_space_type;
+typedef map<string, ExtentSeries*> ExtentSeries_space_type;
+typedef map<string, vector<string> > specs_type;
+
+config_table_type makeTable(std::ifstream &input) {
+  config_table_type table;
+  std::string line;
+  
+  /* Special case for Common fields */
+  config_table_entry_type commonfieldmap;
+  while (getline(input, line)) {
+    vector<string> split_data;
+    boost::split(split_data, line, boost::is_any_of("\t"));
+    
+    if (split_data.size() != 4) {
+      std::cout << "Illegal table file" << std::endl;
+      exit(1);
+    }
+    string extentname = split_data[0];
+    string fieldname = split_data[1];
+    string nullablestr = split_data[2];
+    string fieldtype = split_data[3];
+    
+    bool nullable = false;
+    if (nullablestr == "1")
+      nullable = true;
+    
+    ExtentType::fieldType ftype = ExtentType::ft_unknown;
+    if (fieldtype == "bool")
+      ftype = ExtentType::ft_bool;
+    if (fieldtype == "byte")
+      ftype = ExtentType::ft_byte;
+    if (fieldtype == "int32")
+      ftype = ExtentType::ft_int32;
+    if (fieldtype == "int64" or fieldtype=="time")
+      ftype = ExtentType::ft_int64;
+    if (fieldtype == "double")
+      ftype = ExtentType::ft_double;
+    if (fieldtype == "variable32")
+      ftype = ExtentType::ft_variable32;
+    
+    if (extentname == "Common")
+      commonfieldmap[fieldname] = make_pair(nullable, ftype);
+    else if (table.find(extentname) != table.end())
+      table[extentname] [fieldname] = make_pair(nullable, ftype);
+    else { /* New extent detected */
+      table[extentname] = commonfieldmap;
+      table[extentname] [fieldname] = make_pair(nullable, ftype);
+    }
+  }
+  return table;
+}
+
+specs_type parseSpecs(ifstream &input, config_table_type &table) {
+  string specstring, line;
+  while (getline(input, line)) {
+    // Ignore comments in spec string.
+    if (line.length() > 0 && line[0] != '#') {
+      specstring += line;
+    }
+  }
+  specs_type specs;
+  vector<string> split_data, specdata, fields, commonfields;
+  boost::split(split_data, specstring, boost::is_any_of(";"),
+	       boost::token_compress_on);
+  
+  if (split_data.back() == "")
+    split_data.pop_back();
+  
+  /* Iterate through each extent group */
+  for (vector<string>::iterator split_data_i = split_data.begin();
+       split_data_i != split_data.end();
+       split_data_i++) {
+    
+    /* Break up the extentname and the fields */
+    boost::split(specdata, *split_data_i, boost::is_any_of("\()"));
+    
+    if (specdata.size() < 2) {
+      std::cerr << "Illegal specs string" << std::endl;
+      exit(1);
+    }
+    
+    std::string extentname = specdata[0];
+    boost::split(fields, specdata[1], boost::is_any_of(","),
+		 boost::token_compress_on);
+    if (fields.back() == "")
+      fields.pop_back();
+    
+    if (extentname == "Common")
+      commonfields = fields;
+    else {
+      vector<string> allfields(commonfields);
+      allfields.insert(allfields.end(), fields.begin(), fields.end());
+      
+      /* Check if the name of the extent exist */
+      if (table.find(extentname) == table.end()) {
+	cerr << "Cannot find extent " << extentname << endl;
+	exit(1);
+      }
+      
+      /* Check if all the fields exist*/
+      for (vector<string>::iterator iter = allfields.begin();
+	   iter != allfields.end();
+	   iter++) {
+	if (table[extentname].find(*iter) == table[extentname].end()) {
+	  cerr << "Cannot find field " << extentname << ":" << *iter << endl;
+	  exit(1);
+	}
+      }
+      specs[extentname] = allfields;
+    }
+  }
+  return specs;
+}
+
+class DataSeriesWriteModule {
+public:
+  /* Disable copy constructor */
+  DataSeriesWriteModule(const DataSeriesWriteModule&);
+  
+  /* Constructor to set up all extents and fields */
+  DataSeriesWriteModule(config_table_type &table, specs_type &specs,
+			const std::string xml_dir, const char *outfile) :
+    outfileSink(outfile), table_(table), specs_(specs) {
+    /* Registering extent types to the library */
+    ExtentTypeLibrary extentTypeLibrary;
+    
+    /* Set each extent's size to be 4096 bytes */
+    uint32_t target_extent_size = 4096;
+    
+    /* loop through each extent and create its fields from xmls */
+    for (config_table_type::iterator extent = table_.begin();
+	 extent != table_.end();
+	 extent++) {
+      string extentname = extent->first;
+      
+      /* Loading extent XML descriptions from outside file */
+      std::ifstream myExtentXMLFile((xml_dir + extentname + ".xml").c_str());
+      if (!myExtentXMLFile.is_open()) {
+	std::cout << extentname << ": Coud not open xml file!\n";
+	exit(1);
+      }
+      string myExtentXMLDescription, str;
+      while (getline(myExtentXMLFile, str))
+	myExtentXMLDescription += str + "\n";
+      
+      /* Register the ExtentXMLDescription */
+      const ExtentType &myExtentType =
+	extentTypeLibrary.registerTypeR(myExtentXMLDescription);
+      
+      /* Create ExtentSeries, OutPutModule, and fields */
+      ExtentSeries *myExtentSeries = new ExtentSeries();
+      ExtentSeriesSpace[extentname] = myExtentSeries;
+      OutputModuleSpace[extentname] = new OutputModule(outfileSink, *myExtentSeries,
+						       myExtentType, target_extent_size);
+      fieldSpace.addFields(extentname, *myExtentSeries);
+    }
+    
+    /* Write out the extent type extent. */
+    outfileSink.writeExtentLibrary(extentTypeLibrary);
+  }
+  
+  /* Register the record and field values in string format into fields */
+  void updateRecord(string &record, bool quietmode) {
+    boost::tokenizer<boost::escaped_list_separator<char> > split_data(record);
+    std::vector<string> fields;
+    fields.assign(split_data.begin(),split_data.end());
+    /* First place is the extentname, other fields are values */
+    string extentname = fields[0];
+    if (fields.size() < 2 or specs_[extentname].size() != fields.size() - 1) {
+      cerr << record << ": Invalid record! Number of fields do not match specification string." << endl;
+      exit(1);
+    }
+    
+    OutputModuleSpace[extentname]->newRecord();
+    
+    /* Create a map from fieldnames to fieldvalue. 
+       Iterate through every possible fields (via table_). 
+       If the field is in the map, then set value of the
+       field. Otherwise set it to null 
+    */
+    map<string,string> fieldvaluemap;
+    for (size_t i = 0; i != specs_[extentname].size(); i++) {
+      string fieldname = specs_[extentname][i];
+      string fieldvalue = fields[i+1];
+      fieldvaluemap[fieldname] = fieldvalue;
+    }
+    
+    for (config_table_entry_type::iterator iter = table_[extentname].begin();
+	 iter != table_[extentname].end();
+	 iter++) {
+	string fieldname = iter->first;
+	bool nullable = iter->second.first;
+	if (fieldvaluemap.find(fieldname) != fieldvaluemap.end()) {
+	  string fieldvalue = fieldvaluemap[fieldname];
+	  if (fieldvalue != "") {
+	    fieldSpace.setField(extentname, fieldname, fieldvalue);
+	    continue;
+	  }
+	}
+	if (nullable)
+	  fieldSpace.setNullField(extentname, fieldname);
+	else if (!quietmode) {
+	  cerr << extentname << ":" << fieldname << " ";
+	  cerr << "WARNING: Attempting to setNull to a non-nullable field. ";
+	  cerr << "This field will take on default value instead." << endl;
+	}
+      }
+  }
+    
+  ~DataSeriesWriteModule() {
+    for (OutputModule_space_type::iterator iter = OutputModuleSpace.begin();
+	 iter != OutputModuleSpace.end();
+	 iter++) {
+      iter->second->flushExtent();
+      iter->second->close();
+      delete iter->second;
+    }
+  }
+  
+  OutputModule_space_type OutputModuleSpace;
+  ExtentSeries_space_type ExtentSeriesSpace;
+  FieldSpace fieldSpace;
+  /* Sink is a wrapper for a DataSeries output file. */
+  DataSeriesSink outfileSink;
+private:
+  config_table_type table_;
+  specs_type specs_;
+};
+
+int main(int argc, char *argv[]) {
+  bool quietmode = false;
+  if (argc < 4) {
+    std::cout << "Wrong usage!\n";
+    std::cout << "Usage: " << argv[0] << " [-q] <outputfile> <tablefile> <spec_string_file> <inputfiles...> \n";
+    return 1;
+  }
+  
+  int current_arg = 1;
+  
+  if (!strcmp("-q", argv[current_arg])) {
+    quietmode = true;
+    current_arg++;
+  }
+  
+  const char *outfile = argv[current_arg++];
+  std::ifstream tablefile(argv[current_arg++]);
+  std::ifstream specfile(argv[current_arg++]);
+  std::string xml_dir(argv[current_arg++]);
+  
+  if (!tablefile.is_open()) {
+    std::cout << "Could not open table file!\n";
+    return 1;
+  }
+  
+  if (!specfile.is_open()) {
+    std::cout << "Could not open spec string file!\n";
+    return 1;
+  }
+  
+  /* Load the configuration table */
+  config_table_type table = makeTable(tablefile);
+  
+  /* Parse specification string */
+  specs_type specs = parseSpecs(specfile, table);
+  
+  /* Create Tables and Fields */
+  DataSeriesWriteModule writemod = DataSeriesWriteModule(table, specs, xml_dir,outfile);
+  for (int i = current_arg; i < argc; i++) {
+    std::ifstream inputfile(argv[i]);
+    cout << "Processing input file " << argv[i] << endl;
+    if (!inputfile.is_open()) {
+      cout << "Could not open input trace file!" << endl;
+      continue;
+    }
+    string line;
+    while (getline(inputfile, line))
+      writemod.updateRecord(line, quietmode);
+  }
+}
---
+++ syscall-converter/strace2ds.sh
@@ -0,0 +1,87 @@
+#! /bin/bash
+#
+# Copyright (c) 2015-2016 Leixiang Wu
+# Copyright (c) 2011-2012 Erez Zadok
+# Copyright (c) 2011-2012 Stony Brook University
+# Copyright (c) 2011-2012 The Research Foundation of SUNY
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# strace2ds.sh converts traces in strace format to Dataseries format.
+# The sequence of operations performed by this script is the following:
+# 1. Run strace2csv that converts raw strace to CVS appropriate format
+#    (doing this in C++, not in bash, increases the speed drastically)
+# 2. Use csv2ds to convert CSV to Dataseries format.
+#    Specification of the fields in the input CSV file and the mapping
+#    between CSV fields to Dataseries fields are provided by
+#    specstrings/strace.spec and tables/snia_snia_syscall_fields.table
+#
+# Usage: ./strace2ds.sh <strace_input_file> <ds_output_file>
+#
+
+TABLEFILE=tables/snia_syscall_fields.table
+INPUTFILE=$1
+OUTPUTFILE=$2
+SPECSTRINGFILE=specstrings/strace.spec
+XML_DIR=./xml/
+
+make
+if [ ! -e csv2ds ]; then
+    echo "csv2ds binary is not found. Maybe make it?"
+    exit 1
+fi
+
+if [ ! -e strace2csv ]; then
+    echo "strace2csv binary is not found. Maybe make it?"
+    exit 1
+fi
+
+if [ ! -e $TABLEFILE ]; then
+    echo "system call trace table file is not found! Please put it in $TABLEFILE"
+    exit 1
+fi
+
+if [ ! -e $SPECSTRINGFILE ]; then
+    echo "specification string for strace is not found! Please put it in $SPECSTRINGFILE"
+    exit 1
+fi
+
+if [ ! -e $XML_DIR ]; then
+    ./generate.sh $TABLEFILE
+    echo "Extents XML directory is not found! Please generate them"
+    exit 1
+fi
+
+if [ -z "$INPUTFILE" ]; then
+    echo "You must provide input files!"
+    exit 1
+fi
+
+if [ -z "$OUTPUTFILE" ]; then
+    echo "You must provide the output file name as the second argument!"
+    exit 1
+fi
+
+TEMPFILE="temp-file.$$"
+touch $TEMPFILE
+echo "Using temporary file" $TEMPFILE
+
+echo "Converting strace to CSV..."
+./strace2csv $INPUTFILE $TEMPFILE
+
+if [ $? -ne 0 ]; then
+    rm -f $TEMPFILE
+    exit $?
+fi
+
+echo "Converting strace to CSV - Done"
+
+echo "Converting CSV to DataSeries..."
+./csv2ds -q $OUTPUTFILE $TABLEFILE $SPECSTRINGFILE $XML_DIR $TEMPFILE
+
+echo "Removing temporary file" $TEMPFILE
+rm -f $TEMPFILE
+
+echo "Converting CSV to DataSeries - Done"
