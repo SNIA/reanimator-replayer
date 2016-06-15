26f879123b4211be746109f6945228c9421cfe07
Nina HMC Brown <nbrown@trex.fsl.cs.sunysb.edu>
2016-06-15 14:28:51 -0400

    Fixed comment spacing

--- syscall-replayer/SystemCallTraceReplayModule.hpp
+++ syscall-replayer/SystemCallTraceReplayModule.hpp
@@ -12,13 +12,13 @@
  * This header file provides basic members and functions used internally
  * for implementing a specific system call.
  *
- * SystemCallTraceReplayerModule is an/a abstract class/basic module that 
- * has basic members and functions of a system call. It can be extended 
+ * SystemCallTraceReplayerModule is an/a abstract class/basic module that
+ * has basic members and functions of a system call. It can be extended
  * to implement any specific system call. For example, open, read, write, etc.
- * 
+ *
  * INITIALIZATION AND USAGE
- * As part of the abstraction design, implementing a module that replays 
- * a system call requires a class to extend this abstract class and implement 
+ * As part of the abstraction design, implementing a module that replays
+ * a system call requires a class to extend this abstract class and implement
  * abstract functions - prepareForProcessing(), processRow(), and completeProcessing().
  *
  */
@@ -69,22 +69,22 @@ protected:
 
   /*
    * Compare return value of an operation in trace file and replayed operation
-   * 
+   *
    * @param replayed_ret_val: return value of replayed operation
    */
   void compare_retval_and_errno();
 
   /*
-   * This function will be called before replaying any corresponding 
+   * This function will be called before replaying any corresponding
    * system call.
    * Note: Child class should implement this function to do things
    * before a replayer execute corresponding system call
    */
   virtual void prepareForProcessing() = 0;
 
-  /* 
-   * This function is where all the replaying takes action. 
-   * It will be called by execute() function for each record 
+  /*
+   * This function is where all the replaying takes action.
+   * It will be called by execute() function for each record
    * of a corresponding system call.
    * Note: Child class should implement this function to replay
    * a specific system call
@@ -100,9 +100,9 @@ protected:
    * you want to do after replaying a system call.
    */
   virtual void after_sys_call();
-  
+
   /* 
-   * This function will be called after all system call 
+   * This function will be called after all system call
    * operations are being replayed.
    * Note: Child class can implement this function to do things
    * after all system call operations are replayed
@@ -114,18 +114,18 @@ protected:
 public:
   // A mapping of file descriptors in the trace file to actual file descriptors
   static std::map<int, int> fd_map_;
-  
+
   /*
    * Basic Constructor
    *
    * @param source: dataseries module that contains system call traces
-   * @param verbose_flag: a flag to indicate to replay this module in 
+   * @param verbose_flag: a flag to indicate to replay this module in
    *                      verbose mode
    * @param warn_level_flag: a flag to indicate replaying mode
    *
-   */  
+   */
   SystemCallTraceReplayModule(DataSeriesModule &source, bool verbose_flag, int warn_level_flag);
-  
+
   /*
    * Determine whether or not to replay in verbose mode
    *
@@ -220,22 +220,22 @@ public:
    * @return: a pointer to an extent
    */
   Extent::Ptr getSharedExtent();
-  
+
   /*
    * This function will test to see if current extent
    * has more records in it. If there are, that means not all
    * operations are replayed. Replayer still need to call execute()
    * to continue replaying.
    *
-   * @return: true indicates there are more records in the extent and 
+   * @return: true indicates there are more records in the extent and
    * false otherwise.
    */
   bool cur_extent_has_more_record();
-  
+
   /*
    * This function will be called by a replayer to replay
    * one record of corresponding(based on class name) system call
-   * Note: Replayer should call this function until 
+   * Note: Replayer should call this function until
    * there are no more trace record
    */
   void execute();
