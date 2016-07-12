1881c9df1f49cdc94a8fbe4f3121847d840451dc
Nina HMC Brown <nbrown@trex.fsl.cs.sunysb.edu>
2016-07-12 12:26:34 -0400

    Temp add Dup2 module files

---
+++ syscall-replayer/Dup2SystemCallTraceReplayModule.cpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2015-2016 Leixiang Wu
+ * Copyright (c) 2015-2016 Shubhi Rani
+ * Copyright (c) 2015-2016 Sonam Mandal
+ * Copyright (c) 2015-2016 Erez Zadok
+ * Copyright (c) 2015-2016 Stony Brook University
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This file implements all the functions in the
+ * Dup2SystemCallTraceReplayModule header file.
+ *
+ * Read Dup2SystemCallTraceReplayModule.hpp for more information
+ * about this class.
+ */
+
+#include "Dup2SystemCallTraceReplayModule.hpp"
+
+Dup2SystemCallTraceReplayModule::
+Dup2SystemCallTraceReplayModule(DataSeriesModule &source,
+				bool verbose_flag,
+				int warn_level_flag):
+  SystemCallTraceReplayModule(source, verbose_flag, warn_level_flag),
+  old_descriptor_(series, "old_descriptor"),
+  new_descriptor_(series, "new_descriptor") {
+  sys_call_name_ = "dup2";
+}
+
+void Dup2SystemCallTraceReplayModule::print_specific_fields() {
+  std::cout << "old descriptor(" << old_descriptor_.val() << "), ";
+  std::cout << "new descriptor(" << new_descriptor_.val() << ")";
+}
+
+void Dup2SystemCallTraceReplayModule::processRow() {
+  // Get actual file descriptor
+  int old_fd = SystemCallTraceReplayModule::fd_map_[old_descriptor_.val()];
+  int new_fd = new_descriptor_.val();
+
+  replayed_ret_val_ = dup2(old_fd, new_fd);
+
+  /*
+   * Map replayed duplicated file descriptor to traced duplicated
+   * file descriptor
+   */
+  SystemCallTraceReplayModule::fd_map_[return_value()] = replayed_ret_val_;
+}
---
+++ syscall-replayer/Dup2SystemCallTraceReplayModule.hpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2015-2016 Leixiang Wu
+ * Copyright (c) 2015-2016 Shubhi Rani
+ * Copyright (c) 2015-2016 Sonam Mandal
+ * Copyright (c) 2015-2016 Erez Zadok
+ * Copyright (c) 2015-2016 Stony Brook University
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This header file provides members and functions in order to replay
+ * dup2 system call.
+ *
+ * Dup2SystemCallTraceReplayerModule is a class/module that
+ * has members and functions of replaying dup2 system call.
+ *
+ * INITIALIZATION AND USAGE
+ * A main program could initialize this class with a dataseries file
+ * and call execute() function until all extents are processed.
+ *
+ */
+#ifndef DUP2_SYSTEM_CALL_TRACE_REPLAY_MODULE_HPP
+#define DUP2_SYSTEM_CALL_TRACE_REPLAY_MODULE_HPP
+
+#include "SystemCallTraceReplayModule.hpp"
+
+class Dup2SystemCallTraceReplayModule : public SystemCallTraceReplayModule {
+private:
+  /* DataSeries Dup2 System Call Trace Fields */
+  Int32Field old_descriptor_;
+  Int32Field new_descriptor_;
+
+  /*
+   * Print dup2 sys call field values in a nice format
+   */
+  void print_specific_fields();
+
+  /*
+   * This function will gather arguments in the trace file
+   * and replay a dup2 system call with those arguments
+   */
+  void processRow();
+
+public:
+  Dup2SystemCallTraceReplayModule(DataSeriesModule &source,
+				  bool verbose_flag,
+				  int warn_level_flag);
+};
+
+#endif /* DUP2_SYSTEM_CALL_TRACE_REPLAY_MODULE_HPP */
