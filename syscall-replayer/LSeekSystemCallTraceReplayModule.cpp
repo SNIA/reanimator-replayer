98ae90e3b17b301b2055d6d1e5384f5b7afe7cf5
Shubhi Rani <shubhi@trex.fsl.cs.sunysb.edu>
2016-07-22 11:34:10 -0400

    Fixed bug while printing whence field of LSeek system call

--- syscall-replayer/LSeekSystemCallTraceReplayModule.cpp
+++ syscall-replayer/LSeekSystemCallTraceReplayModule.cpp
@@ -32,7 +32,7 @@ LSeekSystemCallTraceReplayModule(DataSeriesModule &source,
 void LSeekSystemCallTraceReplayModule::print_specific_fields() {
   std::cout << "descriptor(" << descriptor_.val() << "), ";
   std::cout << "offset(" << offset_.val() << "), ";
-  std::cout << "whence(" << whence_.val() << ")";
+  std::cout << "whence(" << (unsigned) whence_.val() << ")";
 }
 
 void LSeekSystemCallTraceReplayModule::processRow() {
