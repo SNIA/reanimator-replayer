a846c46647b2bf9cd71d2e8b8e2c2dddd52822fe
Shubhi Rani <shubhi@trex.fsl.cs.sunysb.edu>
2016-07-25 20:27:35 -0400

    Fixed comments in strace2ds library

--- strace2ds-library/strace2ds.cpp
+++ strace2ds-library/strace2ds.cpp
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2016 Nina Brown
  * Copyright (c) 2015-2016 Leixiang Wu
+ * Copyright (c) 2015-2016 Shubhi Rani
  * Copyright (c) 2015-2016 Erez Zadok
  * Copyright (c) 2015-2016 Stony Brook University
  *
@@ -8,22 +9,9 @@
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  *
- * FIX ME: fix following comments
- * 2ds.
- * This program is similar to DataSeries's csv2ds utility, but handles
- * extents with different types and nullable fields and is primarily used
- * for converting system call csv traces.
+ * This file implements all the functions in the strace2ds.h header file.
  *
- * Usage: ./csv2ds <outputfile> <tablefile> <spec_string_file>
- *        <xml directory> <inputfiles...>
- *
- * <outputfile>: name of the dataseries output file
- * <tablefile>: name of the table file to refer to
- * <spec_string_file>: name of the file that contains a string
- *                    that specifies the format of the input file
- * <xml directory>: directory path that contains extent xml.
- *                 Remember to '/' should be the last character.
- * <inputfiles...>: input CSV files
+ * Read strace2ds.h file for more information about this file.
  */
 
 #include "strace2ds.h"
