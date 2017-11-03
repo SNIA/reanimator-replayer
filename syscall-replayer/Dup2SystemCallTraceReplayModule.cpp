80512acb1f588a4304dbde99755473eebea18073
Darshan Godhia <darshan@fsl.cs.sunysb.edu>
2017-11-03 01:21:48 -0400

    Added socket simulation support for dup2() system call
    Signed-off-by: Darshan Godhia <darshan@fsl.cs.sunysb.edu>

--- syscall-replayer/Dup2SystemCallTraceReplayModule.cpp
+++ syscall-replayer/Dup2SystemCallTraceReplayModule.cpp
@@ -46,25 +46,76 @@ void Dup2SystemCallTraceReplayModule::processRow() {
   int old_fd = replayer_resources_manager_.get_fd(pid, old_descriptor_.val());
   int old_fd_flags = replayer_resources_manager_.get_flags(pid, old_descriptor_.val());
   int new_fd = new_descriptor_.val();
-  int replayed_new_fd = new_fd;
+  int replayed_new_fd = SYSCALL_SIMULATED;
+
   /*
-   * The first is to duplicate onto a descriptor that's
-   * currently open, usually as part of redirecting
-   * stdout or stderr. We can't use new_fd in trace file
-   * and need to replace that with our replayed fd.
-   * Also, dup2 silently closes the file descriptor newfd if
-   * it was previously open. Therefore, we need to
-   * update our mapping.
+   *
+   * To support simulation in cases of FDs originating from sockets,
+   * there are 6 possible non failure cases.
+   *
+   * +-----------+------------------+--------+
+   * |  old_fd   |      new_fd      | action |
+   * +-----------+------------------+--------+
+   * | SIMULATED | SIMULATED        |      1 |
+   * | SIMULATED | Valid & Open     |      1 |
+   * | SIMULATED | Valid & Not open |      1 |
+   * | Valid     | SIMULATED        |      2 |
+   * | Valid     | Valid & Open     |      3 |
+   * | Valid     | Valid & Not open |      4 |
+   * +-----------+------------------+--------+
+   *
+   * Actions:
+   * 1. We need to simulate dup2 on SYSCALL_SIMULATED as old_fd.
+   * If new_fd existed in fd_map, we remove it; since dup2 silently closes
+   * any open new_fd.
+   * Whatever the value of new_fd, we add a mapping of
+   * new_fd <-> SYSCALL_SIMULATED to fd_map.
+   * We do not generate an fd, irrespective of the new_fd value.
+   *
+   * 2. Dup2 has to silently close new_fd and reuse it.
+   * We do remove SYSCALL_SIMULATED from fd_map, but cannot reuse
+   * SYSCALL_SIMULATED.
+   * Dup2 needs to be replayed here. Hence we generate an fd, then
+   * add new_fd <-> generated fd to fd_map.
+   *
+   * 3. Here, new_fd exists in fd_map, so first remove it.
+   * Reuse the new_fd mapping to replay dup2 (this avoids an expensive
+   * call to generate fd).
+   * Re-insert new_fd mapping in the fd_map.
+   *
+   * 4. Here, new_fd does not exists in fd_map.
+   * Generate an unused fd. Replay dup2, and insert
+   * new_fd <-> generated fd into fd_map.
    */
+
+   // In all 4 actions above, if new_fd exists in fd_map, we remove it.
   if (replayer_resources_manager_.has_fd(pid, new_fd)) {
     replayed_new_fd = replayer_resources_manager_.get_fd(pid, new_fd);
     replayer_resources_manager_.remove_fd(pid, new_fd);
-  } else {
-    replayed_new_fd = replayer_resources_manager_.generate_unused_fd(pid);
   }
 
   // The two file descriptors do not share file descriptor flags (the close-on-exec flag).
   int new_fd_flags = old_fd_flags & ~O_CLOEXEC;
+
+  if (old_fd == SYSCALL_SIMULATED) {
+    /*
+     * old_fd originated from a socket() system call.
+     * Add SYSCALL_SIMULATED as replayed_new_fd in the mapping.
+     * Return the replayed return value.
+     * Action: 1
+     */
+
+    replayed_ret_val_ = return_value_.val();
+    replayer_resources_manager_.add_fd(pid, replayed_ret_val_,
+				       SYSCALL_SIMULATED, new_fd_flags);
+    return;
+  }
+
+  if (replayed_new_fd == SYSCALL_SIMULATED) {
+    // Action: 2, 4
+    replayed_new_fd = replayer_resources_manager_.generate_unused_fd(pid);
+  }
+
   replayed_ret_val_ = dup2(old_fd, replayed_new_fd);
 
   // Map replayed duplicated file descriptor to traced duplicated file descriptor
