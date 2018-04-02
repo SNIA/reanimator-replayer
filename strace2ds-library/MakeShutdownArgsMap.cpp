434e637e08352c2e802d6902d42bc1a71dedc848
Siddesh Shinde <sshindesiddesh@gmail.com>
2018-04-02 13:06:38 -0400

    [trace2model] Add support for shutdown(2) system call.

---
+++ strace2ds-library/MakeShutdownArgsMap.cpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2015-2018 Erez Zadok
+ * Copyright (c) 2015-2018 Stony Brook University
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This file implements one of the functions in the DataSeriesOutputModule.hpp
+ * header file.
+ *
+ * Read the DataSeriesOutputModule.hpp file for more information about this
+ * class.
+ */
+
+#include "DataSeriesOutputModule.hpp"
+
+void DataSeriesOutputModule::makeShutdownArgsMap(void **args_map,
+                                             long *args,
+                                             void **v_args) {
+	args_map[SYSCALL_FIELD_DESCRIPTOR] = &args[0];
+	args_map[SYSCALL_FIELD_HOW] = &args[1];
+}
