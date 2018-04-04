6a187b3ad1463639e3881d6833d6d7aef21ff39c
Maryia Maskaliova <maryia@crossroads.fsl.cs.sunysb.edu>
2018-04-04 12:50:11 -0400

    fixed error in makeListenArgsMap()

--- strace2ds-library/MakeListenArgsMap.cpp
+++ strace2ds-library/MakeListenArgsMap.cpp
@@ -19,5 +19,5 @@ void DataSeriesOutputModule::makeListenArgsMap(void **args_map,
                                              long *args,
                                              void **v_args) {
 	args_map[SYSCALL_FIELD_DESCRIPTOR] = &args[0];
-	args_map[SYSCALL_FIELD_BACKLOG] = &v_args[1];
+	args_map[SYSCALL_FIELD_BACKLOG] = &args[1];
 }
