9bcf142d6bd4bd4f408453f3f6e854bdd22ae945
Umit <ibrahim.akgun@stonybrook.edu>
2018-10-15 15:28:40 -0400

    start and end time changes

--- concept-implementations/cv.cpp
+++ concept-implementations/cv.cpp
@@ -1,6 +1,7 @@
 // Copyright Umit
 
 #include <atomic>
+#include <boost/thread/barrier.hpp>
 #include <condition_variable>
 #include <iostream>
 #include <mutex>
@@ -10,42 +11,51 @@
 #include <unordered_map>
 #include <utility>
 #include <vector>
-#include <boost/thread/barrier.hpp>
+#include <chrono>
+#include <random>
 
 std::mutex mtxQueue;
+std::condition_variable cvEnter;
 std::mutex mtxVector;
-std::condition_variable cv;
+std::condition_variable cvExit;
+std::mutex correctVector;
+
 std::atomic<int> data(0);
-bool processed = false;
 
 class Syscalls {
- public:
+public:
   int uniqueId;
   int pid;
-  int timeline;
+  int startTime;
+  int endTime;
 
-  Syscalls(int id, int tid, int timeline)
-      : pid(tid), uniqueId(id), timeline(timeline) {}
+  Syscalls(int id, int tid, int startTime, int endTime)
+      : pid(tid), uniqueId(id), startTime(startTime), endTime(endTime) {}
 };
 
 class ComparePair {
- public:
+public:
   bool operator()(Syscalls *t1, Syscalls *t2) {
     return t1->uniqueId >= t2->uniqueId;
   }
 };
 
-std::priority_queue<Syscalls *, std::vector<Syscalls *>, ComparePair> pq;
 std::unordered_map<std::thread::id, int> tidMap;
+std::priority_queue<Syscalls *, std::vector<Syscalls *>, ComparePair> pq;
+std::vector<Syscalls *> currentRunning;
 
 boost::barrier bar(3);
+
+std::random_device rd;
+std::mt19937 gen(rd());
+std::uniform_int_distribution<> dis(1, 10);
 
 void worker_thread() {
   bar.wait();
   auto tid = tidMap[std::this_thread::get_id()];
   while (1) {
     std::unique_lock<std::mutex> lock(mtxQueue);
-    cv.wait(lock, [&] { return pq.empty() || (pq.top()->pid == tid); });
+    cvEnter.wait(lock, [&] { return pq.empty() || (pq.top()->pid == tid); });
 
     if (pq.empty()) {
       return;
@@ -55,18 +65,43 @@ void worker_thread() {
     auto topPid = top->pid;
     pq.pop();
 
-    lock.unlock();
-    cv.notify_all();
+    std::unique_lock<std::mutex> lockVector(mtxVector);
+    cvExit.wait(lockVector, [&] {
+      for (auto s : currentRunning) {
+        if (s->endTime < top->startTime) {
+          return false;
+        }
+      }
+      return true;
+    });
 
+    currentRunning.push_back(top);
+
+    lock.unlock();
+    cvEnter.notify_all();
+
+    std::cout << topPid << " " << top->startTime << " " << top->endTime << "\n";
+    std::this_thread::sleep_for(std::chrono::milliseconds(dis(gen)));
     data += topPid;
+
+    currentRunning.erase(
+        std::remove(currentRunning.begin(), currentRunning.end(), top),
+        currentRunning.end());
+
+    lockVector.unlock();
+    cvExit.notify_all();
   }
 }
 
 int main() {
-  pq.push(new Syscalls(1, 1, 1));
-  pq.push(new Syscalls(2, 2, 1));
-  pq.push(new Syscalls(3, 1, 2));
-  pq.push(new Syscalls(4, 2, 3));
+  pq.push(new Syscalls(1, 1, 1, 3));
+  pq.push(new Syscalls(2, 2, 1, 3));
+  pq.push(new Syscalls(3, 1, 2, 4));
+  pq.push(new Syscalls(4, 2, 3, 5));
+  pq.push(new Syscalls(5, 1, 4, 6));
+  pq.push(new Syscalls(6, 2, 5, 6));
+  pq.push(new Syscalls(7, 1, 5, 5));
+  pq.push(new Syscalls(8, 2, 6, 8));
 
   std::thread worker(worker_thread);
   tidMap[worker.get_id()] = 1;
@@ -74,12 +109,12 @@ int main() {
   tidMap[worker2.get_id()] = 2;
   bar.wait();
 
-  { std::lock_guard<std::mutex> lock(mtxQueue); }
-  cv.notify_all();
+  // { std::lock_guard<std::mutex> lock(mtxQueue); }
+  // cvEnter.notify_all();
 
   {
     std::unique_lock<std::mutex> lock(mtxQueue);
-    cv.wait(lock, [] { return pq.empty(); });
+    cvEnter.wait(lock, [] { return pq.empty(); });
   }
 
   worker.join();
