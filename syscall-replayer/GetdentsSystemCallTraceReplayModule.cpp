f4030899b1f762a637f28d482971f1d5af5c7bf5
Umit <ibrahim.akgun@stonybrook.edu>
2018-12-28 01:19:41 -0500

    fixing verification for getdents

--- syscall-replayer/GetdentsSystemCallTraceReplayModule.cpp
+++ syscall-replayer/GetdentsSystemCallTraceReplayModule.cpp
@@ -19,6 +19,8 @@
 
 #include "GetdentsSystemCallTraceReplayModule.hpp"
 
+#define getdents(fd, buffer, count) syscall(SYS_getdents, fd, buffer, count)
+
 GetdentsSystemCallTraceReplayModule::GetdentsSystemCallTraceReplayModule(
     DataSeriesModule &source, bool verbose_flag, bool verify_flag,
     int warn_level_flag)
@@ -56,14 +58,13 @@ void GetdentsSystemCallTraceReplayModule::processRow() {
     return;
   }
   struct dirent *buffer = (struct dirent *)new char[count];
-  if (buffer == NULL) {
+  if (buffer == nullptr) {
     replayed_ret_val_ = ENOMEM;
   } else {
-    // TODO (Umit) change this to getdents
-    replayed_ret_val_ = syscall(SYS_getdents, fd, buffer, count);
+    replayed_ret_val_ = (int64_t)getdents(fd, buffer, count);
   }
 
-  if (verify_ == true) {
+  if (verify_) {
     // Verify dirent buffer data and data in the trace file are same
     if (replayed_ret_val_ != return_val ||
         (replayed_ret_val_ > 0 &&
@@ -74,8 +75,14 @@ void GetdentsSystemCallTraceReplayModule::processRow() {
         syscall_logger_->log_warn(
             "time called: ",
             boost::format(DEC_PRECISION) % Tfrac_to_sec(time_called()),
-            "Captured getdents data is different from replayed ",
+            " captured getdents data is different from replayed ",
             "getdents data.");
+        syscall_logger_->log_warn("replayed ret val ", replayed_ret_val_,
+                                  " ret val ", return_val);
+
+        syscall_logger_->log_warn("Captured dirents");
+        printDirents(dirent_buffer_val);
+        printDirents(buffer);
         if (abort_mode()) {
           abort();
         }
@@ -87,15 +94,17 @@ void GetdentsSystemCallTraceReplayModule::processRow() {
     }
     delete[] dirent_buffer_val;
   }
-  if (buffer != NULL) delete[] buffer;
+  if (buffer != nullptr) {
+    delete[] buffer;
+  }
 }
 
 void GetdentsSystemCallTraceReplayModule::prepareRow() {
-  traced_fd = descriptor_.val();
-  count_val = count_.val();
-  return_val = return_value_.val();
+  traced_fd = (unsigned int)descriptor_.val();
+  count_val = (unsigned int)count_.val();
+  return_val = (int64_t)return_value_.val();
 
-  if (verify_ == true) {
+  if (verify_) {
     auto dataBuf = reinterpret_cast<const char *>(dirent_buffer_.val());
     dirent_buffer_val = (struct dirent *)new char[count_val];
     std::memcpy(dirent_buffer_val, dataBuf, count_val);
--- syscall-replayer/GetdentsSystemCallTraceReplayModule.hpp
+++ syscall-replayer/GetdentsSystemCallTraceReplayModule.hpp
@@ -27,7 +27,7 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 #include <cerrno>
-
+#include <string>
 #include "SystemCallTraceReplayModule.hpp"
 
 class GetdentsSystemCallTraceReplayModule : public SystemCallTraceReplayModule {
@@ -38,40 +38,88 @@ class GetdentsSystemCallTraceReplayModule : public SystemCallTraceReplayModule {
   Variable32Field dirent_buffer_;
   Int32Field count_;
 
-  int traced_fd;
+  unsigned int traced_fd;
   struct dirent *dirent_buffer_val;
-  int count_val;
-  int return_val;
+  unsigned int count_val;
+  int64_t return_val;
 
   /**
    * Print getdents sys call field values in a nice format
    */
-  void print_specific_fields();
+  void print_specific_fields() override;
 
   /**
    * This function will gather arguments in the trace file
    * and then replay a getdents system call with those arguments.
    */
-  void processRow();
+  void processRow() override;
+
+  void printDirents(struct dirent *direntBuf) {
+    for (int pos = 0; pos < return_val;) {
+      auto direntRec =
+          (struct dirent *)(reinterpret_cast<char *>(direntBuf) + pos);
+      auto inode = direntRec->d_ino;
+      auto type = *((reinterpret_cast<char *>(direntBuf)) + pos +
+                    direntRec->d_reclen - 1);
+      auto typeStr = getDirentTypeStr(type);
+      auto offset = direntRec->d_off;
+      auto recordLen = direntRec->d_reclen;
+      auto direntName = direntRec->d_name;
+      syscall_logger_->log_warn("inode: ", inode, " type: ", typeStr,
+                                " off: ", offset, " record length: ", recordLen,
+                                " name: ", direntName - 1);
+      pos += recordLen;
+    }
+  }
+
+  std::string getDirentTypeStr(char type) {
+    switch (type) {
+      case DT_REG: {
+        return "regular";
+      }
+      case DT_DIR: {
+        return "directory";
+      }
+      case DT_FIFO: {
+        return "fifo";
+      }
+      case DT_SOCK: {
+        return "socket";
+      }
+      case DT_LNK: {
+        return "symlink";
+      }
+      case DT_BLK: {
+        return "block dev";
+      }
+      case DT_CHR: {
+        return "char dev";
+      }
+      default:
+        return "unknown";
+    }
+  }
 
  public:
   GetdentsSystemCallTraceReplayModule(DataSeriesModule &source,
                                       bool verbose_flag, bool verify_flag,
                                       int warn_level_flag);
-  SystemCallTraceReplayModule *move() {
+  SystemCallTraceReplayModule *move() override {
     auto movePtr = new GetdentsSystemCallTraceReplayModule(
         source, verbose_, verify_, warn_level_);
-    movePtr->setMove(traced_fd, dirent_buffer_val, count_val);
+    movePtr->setMove(traced_fd, dirent_buffer_val, count_val, return_val);
     movePtr->setCommon(uniqueIdVal, timeCalledVal, timeReturnedVal,
                        timeRecordedVal, executingPidVal, errorNoVal, returnVal,
                        replayerIndex);
     return movePtr;
   }
-  void setMove(int fd, struct dirent *direntBuffer, int count) {
+  void setMove(int fd, struct dirent *direntBuffer, int count,
+               int return_val_) {
     traced_fd = fd;
     dirent_buffer_val = direntBuffer;
     count_val = count;
+    return_val = return_val_;
   }
-  void prepareRow();
+  void prepareRow() override;
 };
 #endif /* READ_SYSTEM_CALL_TRACE_REPLAY_MODULE_HPP */
