ee3e458e0504944fe4c962b97b3674160826c0d5
Yinuo Zhang <yinuo@fsl.cs.sunysb.edu>
2019-10-25 15:58:27 -0400

    delete and insert vm_node

--- syscall-replayer/VirtualAddressSpace.cpp
+++ syscall-replayer/VirtualAddressSpace.cpp
@@ -11,6 +11,12 @@ VM_mamager* VM_mamager::getInstance() {
   return instance;
 }
 
+//insert new VM_node to existing vm area
+void VM_area::insert_VM_node(VM_node *node){
+  vma.push_back(node);
+  return;
+}
+
 //can be used to find the left overlapping nodes
 std::vector<VM_node*>* VM_area::find_VM_node(void* addr) {
   auto addr_int = reinterpret_cast<uint64_t>(addr);
@@ -70,13 +76,57 @@ std::vector<VM_node*>* VM_area::find_enclosing_target(void *addr, size_t size){
 
 
 bool VM_area::delete_VM_node(void *addr, size_t size){
-  //find vm regions that need to be modified
+  //find vm regions that need to be modifieda
+  auto addr_int = reinterpret_cast<uint64_t>(addr);
   std::vector<VM_node*>* left_overlap = find_VM_node(addr);
   std::vector<VM_node*>* right_overlap = find_right_overlapping_target(addr, size);
   std::vector<VM_node*>* enclosing = find_enclosing_target(addr, size);
   std::vector<VM_node*>* enclosed = find_enclosed_target(addr, size);
 
+  int overlapped = 0;
+
   //update accordingly
+  if(left_overlap->size() > 0){
+    for(VM_node* node: *left_overlap)
+      node->map_size = addr_int - reinterpret_cast<uint64_t>(node->traced_start_address);
+    overlapped++;
+  }
   
+  if(right_overlap->size() > 0){
+    for(VM_node* node: *right_overlap){
+      node->traced_start_address = addr;
+      node->map_size = node->map_size - (addr_int - reinterpret_cast<uint64_t>(node->traced_start_address));
+    }
+    overlapped++;
+  }
+
+  //how do I delete from the actual vector??
+  if(enclosing->size() > 0){
+    for(VM_node* node: *enclosing){
+      delete node;
+    }
+    enclosing->clear();
+    overlapped++;
+  }
+
+  if(enclosed->size() > 0){
+    for(VM_node* node: *enclosed){
+      //need to insert a new node
+      size_t new_size = (reinterpret_cast<uint64_t>(node->traced_start_address) + node->map_size) - (reinterpret_cast<uint64_t>(addr) + size);
+
+      //my assumption is fd should be the same, need checking
+      VM_node* new_node = new VM_node((void *)(reinterpret_cast<uint64_t>(addr) + size), NULL, new_size, node->traced_fd, node->replayed_fd);
+      insert_VM_node(new_node);
+
+      //update the existing node
+      node->map_size = reinterpret_cast<uint64_t>(addr) - reinterpret_cast<uint64_t>(node->traced_start_address);
+    }
+    overlapped++;
+}
+
+if(overlapped == 0)
+  return false;
+else return true;
+
 
 }
