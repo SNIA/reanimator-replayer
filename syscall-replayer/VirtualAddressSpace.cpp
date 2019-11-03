78a3d5ffb552446fb41281c5818e6e4e102f8d7c
Umit <umit@fsl.cs.sunysb.edu>
2019-11-03 16:15:22 -0500

    deletion is implemented

--- syscall-replayer/VirtualAddressSpace.cpp
+++ syscall-replayer/VirtualAddressSpace.cpp
@@ -34,16 +34,18 @@ void VM_area::insert_VM_node(VM_node* node) {
   return;
 }
 
+enum overlaps { left = 4, right = 2, enclosed = 8, enclosing = 1, invalid = 0 };
+
 bool check_left_overlapping(uint64_t addr_int, size_t size, uint64_t start_addr,
                             uint64_t end_addr) {
-  return ((addr_int + size) > end_addr) && (addr_int > start_addr) &&
+  return ((addr_int + size) >= end_addr) && (addr_int > start_addr) &&
          (addr_int < end_addr);
 }
 
 bool check_right_overlapping(uint64_t addr_int, size_t size,
                              uint64_t start_addr, uint64_t end_addr) {
   return ((addr_int + size) > start_addr) && ((addr_int + size) < end_addr) &&
-         (addr_int < start_addr);
+         (addr_int <= start_addr);
 }
 
 bool check_enclosed_overlapping(uint64_t addr_int, size_t size,
@@ -53,7 +55,7 @@ bool check_enclosed_overlapping(uint64_t addr_int, size_t size,
 
 bool check_enclosing_overlapping(uint64_t addr_int, size_t size,
                                  uint64_t start_addr, uint64_t end_addr) {
-  return (addr_int >= start_addr) && ((addr_int + size) <= end_addr);
+  return (addr_int > start_addr) && ((addr_int + size) < end_addr);
 }
 
 std::vector<VM_node*>* VM_area::find_VM_node(void* addr, size_t size) {
@@ -72,19 +74,82 @@ std::vector<VM_node*>* VM_area::find_VM_node(void* addr, size_t size) {
 }
 
 bool VM_area::delete_VM_node(void* addr, size_t size) {
+  auto addr_int = reinterpret_cast<uint64_t>(addr);
+
   vma_lock.lock();
 
   list();
   auto result = find_VM_node(addr, size);
 
-  for (auto r : *result) {
-    if (r != NULL) {
-      SystemCallTraceReplayModule::syscall_logger_->log_info(
-          "addr ",
-          boost::format("%02x") %
-              reinterpret_cast<uint64_t>(r->traced_start_address));
+  for (auto node : *result) {
+    if (node == NULL) continue;
+    SystemCallTraceReplayModule::syscall_logger_->log_info(
+        "addr ",
+        boost::format("%02x") %
+            reinterpret_cast<uint64_t>(node->traced_start_address));
+    // we have only two cases
+    // first delete the node
+    // second just update
+    auto start_addr = reinterpret_cast<uint64_t>(node->traced_start_address);
+    auto end_addr = start_addr + node->map_size;
+
+    int overlapped = 0;
+    overlapped |= check_left_overlapping(addr_int, size, start_addr, end_addr)
+                      ? left
+                      : invalid;
+    overlapped |= check_right_overlapping(addr_int, size, start_addr, end_addr)
+                      ? right
+                      : invalid;
+    overlapped |=
+        check_enclosed_overlapping(addr_int, size, start_addr, end_addr)
+            ? enclosed
+            : invalid;
+    overlapped |=
+        check_enclosing_overlapping(addr_int, size, start_addr, end_addr)
+            ? enclosing
+            : invalid;
+    SystemCallTraceReplayModule::syscall_logger_->log_info("overllaped ",
+                                                           overlapped);
+
+    switch (overlapped) {
+      case enclosed: {
+        vma.erase(std::find(vma.begin(), vma.end(), node));
+        break;
+      }
+      case left: {
+        node->map_size = addr_int - start_addr;
+        break;
+      }
+      case right: {
+        auto new_start_addr = addr_int + size;
+        node->replayed_start_address = reinterpret_cast<void*>(
+            reinterpret_cast<uint64_t>(node->replayed_start_address) +
+            (new_start_addr - start_addr));
+        node->map_size -= (new_start_addr - start_addr);
+        node->traced_start_address = reinterpret_cast<void*>(new_start_addr);
+        break;
+      }
+      case enclosing: {
+        // create a new node
+        auto new_traced_addr = addr_int + size;
+        auto new_replyed_addr =
+            reinterpret_cast<uint64_t>(node->replayed_start_address) +
+            new_traced_addr - start_addr;
+        auto new_size = end_addr - new_traced_addr;
+        auto new_node =
+            new VM_node(reinterpret_cast<void*>(new_traced_addr),
+                        reinterpret_cast<void*>(new_replyed_addr), new_size,
+                        node->traced_fd, node->replayed_fd);
+        vma.push_back(new_node);
+
+        // update the size of the node
+        node->map_size = addr_int - start_addr;
+        break;
+      }
     }
   }
+  result->clear();
+  delete result;
 
   vma_lock.unlock();
   return false;
