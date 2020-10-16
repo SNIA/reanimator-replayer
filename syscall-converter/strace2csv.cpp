1d18ad81129a2abb76ce3166a87c83b82fa4e0ed
Thomas Fleming <tfleming@hmc.edu>
2020-10-16 15:31:52 -0400

    Update all copyright headers in code files

--- syscall-converter/strace2csv.cpp
+++ syscall-converter/strace2csv.cpp
@@ -1,15 +1,25 @@
 /*
- * Copyright (c) 2015-2016 Leixiang Wu
+ * Copyright (c) 2017      Darshan Godhia
+ * Copyright (c) 2016-2019 Erez Zadok
+ * Copyright (c) 2011      Jack Ma
+ * Copyright (c) 2019      Jatin Sood
+ * Copyright (c) 2017-2018 Kevin Sun
+ * Copyright (c) 2015-2017 Leixiang Wu
+ * Copyright (c) 2020      Lukas Velikov
+ * Copyright (c) 2017-2018 Maryia Maskaliova
+ * Copyright (c) 2017      Mayur Jadhav
+ * Copyright (c) 2016      Ming Chen
+ * Copyright (c) 2017      Nehil Shah
+ * Copyright (c) 2016      Nina Brown
+ * Copyright (c) 2011-2012 Santhosh Kumar
  * Copyright (c) 2015-2016 Shubhi Rani
- * Copyright (c) 2015-2016 Sonam Mandal
- * Copyright (c) 2015-2016 Erez Zadok 
- * Copyright (c) 2011-2012 Jack Ma
+ * Copyright (c) 2018      Siddesh Shinde
+ * Copyright (c) 2014      Sonam Mandal
+ * Copyright (c) 2012      Sudhir Kasanavesi
+ * Copyright (c) 2020      Thomas Fleming
+ * Copyright (c) 2018-2020 Ibrahim Umit Akgun
  * Copyright (c) 2011-2012 Vasily Tarasov
- * Copyright (c) 2011-2012 Santhosh Kumar Koundinya
- * Copyright (c) 2011-2012 Geoff Kuenning
- * Copyright (c) 2011-2012 Stony Brook University
- * Copyright (c) 2011-2012 Harvey Mudd College
- * Copyright (c) 2011-2012 The Research Foundation of SUNY
+ * Copyright (c) 2019      Yinuo Zhang
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
